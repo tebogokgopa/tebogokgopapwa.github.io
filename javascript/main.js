6aacadec23178eb862bb047d709d8581e89773df
tebogokgopa <tebogokgopa84@gmail.com>
2023-01-24 17:02:13 +0200

    renamed main image

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,7 @@
 
 
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu bar icon */
-function hambuggermenubarfunction() {
+function hamburgermenubarfunction() {
     var x = document.getElementById("topnavbarlinks");
     if (x.style.display === "block") {
       x.style.display = "none";
